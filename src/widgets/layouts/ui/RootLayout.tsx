7aefac7a680daa97bbf5574a8a07ed0b4efc96b2
Dobbymin <gmin990624@gmail.com>
2025-12-19 12:38:41 +0900

    feat: 전체 레이아웃 설정 구현

--- src/widgets/layouts/ui/RootLayout.tsx
+++ src/widgets/layouts/ui/RootLayout.tsx
@@ -1,15 +1,24 @@
 import { Outlet } from 'react-router';
 
+import { Box, Flex } from '@chakra-ui/react';
+
 import { Header, Navigator } from '../components';
 
 export const RootLayout = () => {
   return (
-    <div>
+    <Flex
+      direction='column'
+      maxW={{ base: '100vw', lg: '900px' }}
+      maxH={{ base: '100vh', md: '750px' }}
+      w='100vw'
+      h='100vh'
+      mx='auto'
+    >
       <Header />
-      <main>
+      <Box w='full' as='main' flex='1'>
         <Outlet />
-      </main>
+      </Box>
       <Navigator />
-    </div>
+    </Flex>
   );
 };
