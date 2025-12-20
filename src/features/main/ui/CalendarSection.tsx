65fdaa9482bb0b94d76e99e2872c7251dfbd1ffd
Dobbymin <gmin990624@gmail.com>
2025-12-20 13:49:22 +0900

    feat: calendar 컴포넌트 추가

---
+++ src/features/main/ui/CalendarSection.tsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+
+export const CalendarSection = () => {
+  return (
+    <Flex w="65%" p={5} bg="purple.200">
+      Calender Section
+    </Flex>
+  );
+};
