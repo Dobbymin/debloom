0e32ce11b1edc2b4f810f0e7e4cef8e0215bb5dd
Dobbymin <gmin990624@gmail.com>
2025-12-22 21:27:32 +0900

    feat: 달력 요일 구현

---
+++ src/features/main/components/features/calendar/calendar-weekdays.tsx
@@ -0,0 +1,19 @@
+import { Flex, SimpleGrid, Text } from "@chakra-ui/react";
+
+type Props = {
+  weekDays: string[];
+};
+
+export const CalendarWeekdays = ({ weekDays }: Props) => {
+  return (
+    <SimpleGrid columns={7} w="full">
+      {weekDays.map((day, idx) => (
+        <Flex key={`${day}-${idx}`} justifyContent="center">
+          <Text color={idx === 0 ? "red.500" : idx === 6 ? "blue.500" : "black"} fontWeight="bold" textAlign="center">
+            {day}
+          </Text>
+        </Flex>
+      ))}
+    </SimpleGrid>
+  );
+};
