1f24b07c3a1c1c9a846fbfccf0653924897ebf3f
Dobbymin <gmin990624@gmail.com>
2025-12-22 21:28:31 +0900

    feat: 달력 컴포넌트 구현 및 컴포넌트 분리

--- src/features/main/ui/CalendarSection.tsx
+++ src/features/main/ui/CalendarSection.tsx
@@ -1,9 +1,32 @@
+import { useState } from "react";
+
 import { Flex } from "@chakra-ui/react";
+import dayjs from "dayjs";
+import isoWeek from "dayjs/plugin/isoWeek";
+import weekOfYear from "dayjs/plugin/weekOfYear";
+import weekday from "dayjs/plugin/weekday";
+
+import { CalendarDays, CalendarHeader, CalendarWeekdays } from "../components";
+import { type MonthChangeAction, getChangedMonth } from "../utils";
 
 export const CalendarSection = () => {
+  dayjs.extend(weekday);
+  dayjs.extend(isoWeek);
+  dayjs.extend(weekOfYear);
+
+  const [viewDate, setViewDate] = useState(dayjs());
+
+  const weekDays = ["일", "월", "화", "수", "목", "금", "토"];
+
+  const changeMonth = (action: MonthChangeAction) => {
+    setViewDate(getChangedMonth(viewDate, action));
+  };
+
   return (
-    <Flex w="65%" p={5} bg="purple.200">
-      Calender Section
+    <Flex w="65%" flexDir="column" gap={5} p={5}>
+      <CalendarHeader viewDate={viewDate} onChangeMonth={changeMonth} />
+      <CalendarWeekdays weekDays={weekDays} />
+      <CalendarDays viewDate={viewDate} />
     </Flex>
   );
 };
