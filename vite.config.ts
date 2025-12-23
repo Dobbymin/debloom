b3e3992f388184fc214d4714ef801558d6d5d04f
Dobbymin <gmin990624@gmail.com>
2025-12-24 04:38:24 +0900

    feat: pwa 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,33 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import react from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react({
       babel: {
-        plugins: [['babel-plugin-react-compiler']],
+        plugins: [["babel-plugin-react-compiler"]],
       },
-      jsxImportSource: '@emotion/react',
+      jsxImportSource: "@emotion/react",
     }),
     tsconfigPaths(),
+    VitePWA({
+      injectRegister: "auto",
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "icon/*.png"],
+      manifest: false,
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,png,svg,ico,json}"],
+        maximumFileSizeToCacheInBytes: 8 * 1024 * 1024, // 필수 x
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
