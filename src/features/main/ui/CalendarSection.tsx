d63c171e4b222dcb066a8978b2967df02f72cd4f
Dobbymin <gmin990624@gmail.com>
2025-12-24 05:35:31 +0900

    fix: 달력 gap 수정

--- src/features/main/ui/CalendarSection.tsx
+++ src/features/main/ui/CalendarSection.tsx
@@ -30,7 +30,7 @@ export const CalendarSection = () => {
   };
 
   return (
-    <Flex w="55%" flexDir="column" gap={4} p={6}>
+    <Flex w="55%" flexDir="column" gap={2} p={6}>
       <UserLink />
       <CalendarHeader viewDate={viewDate} onChangeMonth={changeMonth} />
       <CalendarWeekdays weekDays={weekDays} />
