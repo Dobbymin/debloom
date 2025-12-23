69d215624711df1afa5d7fc84e658a1ec79a80a8
Dobbymin <gmin990624@gmail.com>
2025-12-24 04:55:44 +0900

    fix: pwa 설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,41 @@ export default defineConfig({
       injectRegister: "auto",
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "icon/*.png"],
-      manifest: false,
+      manifest: {
+        name: "debloom",
+        short_name: "debloom",
+        description: "debloom Progressive Web App",
+        theme_color: "#D6DCFF",
+        background_color: "#ffffff",
+        display: "standalone",
+        start_url: "/",
+        icons: [
+          {
+            src: "/icon/apple-touch-icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "/icon/apple-touch-icon-152x152.png",
+            sizes: "152x152",
+            type: "image/png",
+            purpose: "any"
+          },
+          {
+            src: "/icon/favicon-196x196.png",
+            sizes: "196x196",
+            type: "image/png",
+            purpose: "any maskable"
+          },
+          {
+            src: "/icon/mstile-310x310.png",
+            sizes: "310x310",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
+      },
       workbox: {
         globPatterns: ["**/*.{js,css,html,png,svg,ico,json}"],
         maximumFileSizeToCacheInBytes: 8 * 1024 * 1024, // 필수 x
